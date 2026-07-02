15b18f9e0453550427fc5ff2f6d4ba9a7fdf644b
Ali Attash <ali.attash1234@gmail.com>
2026-07-02 09:17:06 +0430

    some new changes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,11 +1,10 @@
 import express, { Application } from "express";
-const app: Application = express();
 import toursRoute from "./routes/toursRoutes";
 import usersRoute from "./routes/usersRoutes";
 import { errRouteHandle } from "./middlewares/errorHandlingMiddleware";
 import { globalErrorMiddleware } from "./middlewares/globalErrorHandlingMiddleware";
 import { AppError } from "./utils/factories/appError";
-
+const app: Application = express();
 app.use(express.json());
 app.use("/api/v1/tours", toursRoute);
 app.use("/api/v1/users", usersRoute);
